cb3002a7cf6360e7bb079890db978dc89b1fa6b8
nik9play <nik9play@cock.li>
2018-10-26 21:23:55 +0300

    remove tabs

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const { app, BrowserWindow } = require('electron')
-  let win
-  
-  function createWindow () {
-    win = new BrowserWindow({ width: 640, height: 360, minWidth: 320, minHeight: 180, frame: false, show: false, backgroundColor: '#15151c', alwaysOnTop: true })
-  
-    win.once('ready-to-show', () => {
-      win.show()
-    })
-    win.loadFile('index.html')
+let win
 
-    //win.webContents.openDevTools()
+function createWindow () {
+  win = new BrowserWindow({ width: 640, height: 360, minWidth: 320, minHeight: 180, frame: false, show: false, backgroundColor: '#15151c', alwaysOnTop: true })
 
-    win.on('closed', () => {
-      win = null
-    })
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+  win.loadFile('index.html')
+
+  //win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// macos shit
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
+})
 
-  app.on('ready', createWindow)
-  
-  // macos shit
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    if (win === null) {
-      createWindow()
-    }
-  })
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
